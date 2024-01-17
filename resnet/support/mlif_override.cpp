9a12162811524166a69938d85f492c932b4050a2
jokap11 <ge25der@mytum.de>
2024-01-17 18:45:08 +0100

    Enable multi output information via memory write

--- resnet/support/mlif_override.cpp
+++ resnet/support/mlif_override.cpp
@@ -24,38 +24,50 @@ int mlif_process_input(const void *in_data, size_t in_size, void *model_input_pt
 
 int mlif_process_output(void *model_output_ptr, size_t model_output_sz, const void *expected_out_data, size_t expected_out_size)
 {
-    if (model_output_sz != 10)
+    static unsigned addr;
+    if (model_output_sz != 10 && model_output_sz != 1)
     {
         DBGPRINTF("MLIF: Wrong output size!\n");
         return EXIT_MLIF_INVALID_SIZE;
-    }
+    }else if (model_output_sz == 10){
 
-    float out_scale = 0.00390625;
-    int out_zeropt = -128;
+        addr = 0x800100; //set once
+        float out_scale = 0.00390625;
+        int out_zeropt = -128;
 
-    float results[10];
-    int prediction = 0;
-    for (int i = 0; i < 10; i++)
-    {
-        results[i] = DequantizeInt8ToFloat(((int8_t*)model_output_ptr)[i], out_scale, out_zeropt);
-        DBGPRINTF("Category %i: %.9g\n", i, results[i]);
-
-        if (results[i] >= results[prediction])
+        float results[10];
+        int prediction = 0;
+        for (int i = 0; i < 10; i++)
         {
-            prediction = i;
+            results[i] = DequantizeInt8ToFloat(((int8_t*)model_output_ptr)[i], out_scale, out_zeropt);
+            DBGPRINTF("Category %i: %.9g\n", i, results[i]);
+
+            if (results[i] >= results[prediction])
+            {
+                prediction = i;
+            }
+        }
+        DBGPRINTF("Predicted category: %i\n", prediction);
+
+        for (int i = 0; i < 10; i++)
+        {
+            float expected = ((float*)expected_out_data)[i];
+            if (results[i] != expected)
+            {
+                DBGPRINTF("MLIF: Wrong output in category %i! Expected %.9g\n", i, expected);
+                return EXIT_MLIF_MISSMATCH;
+            }
         }
     }
-    DBGPRINTF("Predicted category: %i\n", prediction);
 
-
-    for (int i = 0; i < 10; i++)
+    DBGPRINTF("Printing everything after %x :\n", addr);
+    for (size_t i = 0; i < model_output_sz; i++)
     {
-        float expected = ((float*)expected_out_data)[i];
-        if (results[i] != expected)
-        {
-            DBGPRINTF("MLIF: Wrong output in category %i! Expected %.9g\n", i, expected);
-            return EXIT_MLIF_MISSMATCH;
-        }
+        int8_t written_byte = 0xFF; //Check if actually read
+        written_byte = *((int8_t*)model_output_ptr + i);
+        *((volatile int8_t *)addr++) = written_byte;  //Little Endian so 44 is at 0x800100 = 0x11223344
+        DBGPRINTF("Write hex number %#04hhx to memory \n", written_byte);
     }
+
     return 0;
 }
